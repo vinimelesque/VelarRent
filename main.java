0413fc93119cb073f68a91582ff4b212ebb1726a
Vinícius Melesque <101480393+vinimelesque@users.noreply.github.com>
2023-06-25 22:43:34 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xboxd
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame Login = new Login();
+        Login.setVisible(true); //janela login visivel
+        Login.setLocationRelativeTo(null);
+    }
+    
+}
